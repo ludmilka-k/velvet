339134efdf5f0dd17c3e0d3ca716f780c3c10e32
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-03-12 15:49:16 +0200

    Theme mode toggle

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 import {Button} from '@/components/ui/button';
 import {APP_NAME} from "@/lib/constants";
+import ModeToggle from "./mode-toggle";
 
 const Header = () => {
     return (
@@ -22,6 +23,7 @@ const Header = () => {
                     </Link>
                 </div>
                 <div className='space-x-2'>
+                    <ModeToggle />
                     <Button asChild variant='ghost'>
                         <Link href='/cart'>
                             <ShoppingCart />
