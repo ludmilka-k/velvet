cdd38c8197413493efb0085a66d31bb55662aa7c
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-03-14 15:34:31 +0200

    Add Product card component

--- components/shared/prodact/prodact-list.tsx
+++ components/shared/prodact/prodact-list.tsx
@@ -1,4 +1,5 @@
 import { Product } from '@/types';
+import ProductCard from "./product-card";
 
 const ProductList = ({
    data,
@@ -18,7 +19,7 @@ const ProductList = ({
             {data.length > 0 ? (
               <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
                 {limitedData.map((product: Product) => (
-                  <div key={product.id}>{product.name}</div>
+                  <ProductCard key={product.slug} product={product} />
                 ))}
               </div>
             ) : (
--- lib/validators.ts
+++ lib/validators.ts
@@ -3,6 +3,11 @@ import { z } from 'zod';
 // Schema for inserting products
 export const insertProductSchema = z.object ({
     name: z.string(),
+    slug: z.string(),
     description: z.string(),
+    images: z.array(z.string()),
+    brand: z.string(),
+    stock: z.coerce.number(),
+    price: z.number(),
     // more
 });
