8f8d17bed470b84a4bfc5ebf49fa4b3b5e1db63b
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-03-25 18:18:04 +0200

    Remove from cart action

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -143,3 +143,58 @@ export async function getMyCart() {
         taxPrice: cart.taxPrice.toString(),
     });
 }
+
+export async function removeItemFromCart(productId: string) {
+    try {
+      // Check for cart cookie
+      const sessionCartId = (await cookies()).get('sessionCartId')?.value;
+      if (!sessionCartId) throw new Error('Session Cart not found');
+
+      // Get Product
+      const product = await prisma.product.findFirst({
+        where: {id: productId},
+      });
+      if (!product) throw new Error('Product not found');
+
+      // Get user cart
+      const cart = await getMyCart();
+      if(!cart) throw new Error('Cart not found');
+
+      // Check for item
+        const existItem = (cart.items as CartItem[])
+          .find((x) => x.productId === productId);
+        if (!existItem) throw new Error('Item not found');
+
+        // Check if only one in qty
+        if (existItem.qty === 1) {
+          // Remove from cart
+          cart.items = (cart.items as CartItem[])
+            .filter((x) => x.productId !== existItem.productId);
+        } else {
+          // Decrease qty
+          (cart.items as CartItem[])
+            .find((x) => x.productId === productId)!.qty = existItem.qty -1;
+        }
+
+        // Update cart in database
+        await prisma.cart.update({
+          where: {id: cart.id},
+          data: {
+            items: cart.items as Prisma.CartUpdateitemsInput[],
+            ...calcPrice(cart.items as CartItem[])
+          }
+        });
+
+        // Revalidate product page
+        revalidatePath(`/product/${product.slug}`);
+
+    return {
+      success: true,
+      message: `${product.name}  ${
+        (cart.items as CartItem[]).find((x) => x.productId === productId)
+          ? 'updated in' : 'removed from'} cart`,
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
