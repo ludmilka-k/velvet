a6270dffd35b4660645086c7c97b5657d2341206
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-03-25 19:06:31 +0200

    Dynamic cart button

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,16 +2,27 @@
 
 import { Button } from '@/components/ui/button';
 import {useRouter} from 'next/navigation';
-import {Plus} from 'lucide-react';
-import {CartItem} from '@/types';
+import {Plus, Minus} from 'lucide-react';
+import {Cart, CartItem} from '@/types';
 import {useToast} from '@/hooks/use-toast';
 import {ToastAction} from '@/components/ui/toast';
-import {addItemToCart} from '@/lib/actions/cart.actions';
+import {addItemToCart, removeItemFromCart} from '@/lib/actions/cart.actions';
 
-const AddToCart = ({item}:{item:CartItem}) => {
+
+const AddToCart = ({cart, item}:{cart?: Cart, item: CartItem}) => {
     const router = useRouter();
     const {toast} = useToast();
 
+    const handleRemoveFromCart = async () => {
+        const res = await removeItemFromCart(item.productId);
+
+        toast({
+            variant: res.success ? 'default' : 'destructive',
+            description: res.message,
+        });
+
+        return;
+    }
     const handleAddToCart = async () => {
         const res = await addItemToCart(item);
 
@@ -33,11 +44,25 @@ const AddToCart = ({item}:{item:CartItem}) => {
                 Go To Cart
             </ToastAction>
         })
-    }
+    };
 
-    return (
+    // Check if item is in the cart
+    const exist = cart && cart.items
+        .find((x) => x.productId === item.productId);
+
+    return exist ? (
+        <div>
+          <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
+            <Minus className='h-4, w-4'/>
+          </Button>
+          <span className='px-2'>{exist.qty}</span>
+          <Button type='button' variant='outline' onClick={handleAddToCart}>
+            <Plus className='w-4 h-4' />
+          </Button>
+        </div>
+      ) : (
         <Button className='w-full' type='button' onClick={handleAddToCart}>
-           <Plus /> Add To Cart
+          <Plus className='w-4 h-4'/> Add to cart
         </Button>
     )
 }
