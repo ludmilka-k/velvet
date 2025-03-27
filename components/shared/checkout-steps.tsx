2f8b8cd561d5d4a66196ce50dce732318b9f294c
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-03-27 11:56:32 +0200

    Add CheckoutSteps component

---
+++ components/shared/checkout-steps.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {cn} from "@/lib/utils";
+
+const CheckoutSteps = ({current = 0}) => {
+    return (
+      <div className='flex-between flex-col md:flex-row space-x-2 space-y-2 mb-10'>
+        {['User Login', 'Shipping Address', 'Payment Method', 'Place Order'].map((step, index) => (
+          <React.Fragment key={step}>
+            <div className={cn('p-2 w-56 rounded-full text-center  text-sm', index === current
+                ? 'bg-secondary' : '')}
+            >
+                {step}
+            </div>
+          </React.Fragment>
+        )) }
+      </div>
+    )
+}
+
+export default CheckoutSteps
