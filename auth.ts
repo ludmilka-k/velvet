a1531ebe5d0f28b6170f40c1a388e61dbbad3ed4
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-03-27 12:29:08 +0200

    Persist session cart to user

--- auth.ts
+++ auth.ts
@@ -3,9 +3,10 @@ import NextAuth from 'next-auth';
 import { PrismaAdapter} from '@auth/prisma-adapter';
 import { prisma } from '@/db/prisma';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import {compareSync} from "bcrypt-ts-edge";
+import {compareSync} from 'bcrypt-ts-edge';
 import type {NextAuthConfig} from 'next-auth';
 import {NextResponse} from 'next/server';
+import {cookies} from 'next/headers';
 
 export const config = {
   pages: {
@@ -74,19 +75,42 @@ export const config = {
         if (user.name === 'NO_NAME') {
           token.name = user.email!.split('@')[0];
 
-          //Update database to reflect the token name
+          // Update database to reflect the token name
           await prisma.user.update({
             where: {id: user.id},
             data: {name: token.name},
           });
-      }
+        }
 
-        // Handle session updates
-        if (session?.user.name && trigger === 'update') {
-          token.name = session.user.name;
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: {sessionCartId},
+            });
+
+            if (sessionCart) {
+              //Delete current user cart
+              await prisma.cart.deleteMany({
+                where: {userId: user.id}
+              });
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: {id: sessionCart.id},
+                data: {userId: user.id}
+              });
+            }
+          }
         }
       }
 
+      // Handle session updates
+      if (session?.user.name && trigger === 'update') {
+        token.name = session.user.name;
+      }
 
       return token;
     },
