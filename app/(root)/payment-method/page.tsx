19c06f8c6e05e4b52f4a6ff4564eb1020d80b03b
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-03-28 11:39:21 +0200

    Payment method form and submission

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -2,6 +2,7 @@ import {Metadata} from 'next';
 import {auth} from '@/auth';
 import {getUserById} from '@/lib/actions/user.actions';
 import PaymentMethodForm from './payment-method-form';
+import CheckoutSteps from "@/components/shared/checkout-steps";
 
 export const metadata: Metadata = {
     title: 'Select Payment Method',
@@ -16,7 +17,10 @@ const PaymentMethodPage = async () => {
     const user = await getUserById(userId);
 
     return (
+      <>
+        <CheckoutSteps current={2} />
         <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
+      </>
     )
 };
 
