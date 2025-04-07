b6aa972476e5aa248933dc2b257d6432ae52a52d
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-04-08 00:14:18 +0300

    Implement Paypal Buttons

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -13,8 +13,15 @@ import {Order} from '@/types';
 import {formatCurrency, formatDateTime, formatId} from '@/lib/utils';
 import Link from 'next/link';
 import Image from 'next/image';
+import {
+    PayPalButtons,
+    PayPalScriptProvider,
+    usePayPalScriptReducer,
+} from '@paypal/react-paypal-js';
+import {approvePayPalOrder, createPayPalOrder} from '@/lib/actions/order.actions';
+import {useToast} from "@/hooks/use-toast";
 
-const OrderDetailsTable = ({order}: {order: Order}) => {
+const OrderDetailsTable = ({order, paypalClientId}: {order: Order, paypalClientId: string}) => {
       const {
         shippingAddress,
         orderItems,
@@ -28,6 +35,41 @@ const OrderDetailsTable = ({order}: {order: Order}) => {
         isDelivered,
         deliveredAt,
       } = order;
+
+      const {toast} = useToast();
+
+      const PrintLoadingState = () => {
+        const [{isPending, isRejected}] = usePayPalScriptReducer();
+        let status = '';
+
+        if (isPending) {
+          status = 'Loading PayPal ...'
+        } else if (isRejected) {
+          status = 'Error Loading PayPal ...'
+        }
+        return status;
+      };
+
+      const handleCreatePayPalOrder = async () => {
+        const res = await createPayPalOrder(order.id);
+
+        if (!res.success) {
+          toast ({
+            variant: 'destructive',
+            description: res.message,
+          });
+        }
+        return res.data;
+      }
+
+      const handleApprovePayPalOrder = async (data: {orderID: string}) => {
+        const res = await approvePayPalOrder(order.id, data);
+
+        toast({
+            variant:res.success ? 'default' : 'destructive',
+            description: res.message,
+        })
+      }
 
     return (
       <>
@@ -123,6 +165,18 @@ const OrderDetailsTable = ({order}: {order: Order}) => {
                     <h2 className='text-normal'>Total</h2>
                     <div>{formatCurrency(totalPrice)}</div>
                   </div>
+                    {/* PayPal Payment */}
+                  {!isPaid && paymentMethod === 'PayPal' && (
+                    <div>
+                      <PayPalScriptProvider options={{clientId: paypalClientId}}>
+                        <PrintLoadingState />
+                        <PayPalButtons
+                          createOrder={handleCreatePayPalOrder}
+                          onApprove={handleApprovePayPalOrder}
+                        />
+                      </PayPalScriptProvider>
+                    </div>
+                  )}
                 </CardContent>
               </Card>
             </div>
