c863b4b7dca62da01a696d63af59a8bd60d57c17
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-04-11 13:20:43 +0300

    Admin layout and menu

---
+++ app/admin/layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {APP_NAME} from '@/lib/constants';
+import Link from 'next/link';
+import Image from 'next/image';
+import Menu from '@/components/shared/header/menu';
+import AdminMainNav from './main-nav';
+import {Input} from '@/components/ui/input';
+
+export default function AdminLayout({children}: {children: React.ReactNode}) {
+  return (
+    <div className='flex flex-col'>
+      <div className='border-b container mx-auto'>
+        <div className='flex h-16 items-center px-4'>
+          <Link href='/' className='w-22'>
+            <Image src='/images/logo.svg' alt={`${APP_NAME}Logo`} width={48} height={48} />
+          </Link>
+          <AdminMainNav className='mx-6'/>
+          <div className='ml-auto flex items-center space-x-4'>
+            <div>
+              <Input type='search' placeholder='Search...' className='lg:w-[300px]'/>
+            </div>
+            <Menu />
+          </div>
+        </div>
+      </div>
+
+      <div className='flex-1 space-y-4 p-8 pt-6 container mx-auto'>{children}</div>
+    </div>
+  )
+}
---
+++ app/admin/overview/page.tsx
@@ -0,0 +1,13 @@
+import {Metadata} from 'next';
+
+export const metadata:Metadata = {
+    title: 'Admin Overview',
+}
+
+const AdminOverviewPage = () => {
+    return (
+        <>Overview</>
+    )
+};
+
+export default AdminOverviewPage;
