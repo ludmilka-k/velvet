0cea00b5e289e390aae572c0cc11b1e1e6e8bff1
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-04-11 17:05:53 +0300

    Get Order Summary action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -9,8 +9,9 @@ import {insertOrderSchema} from '@/lib/validators';
 import {prisma} from '@/db/prisma';
 import {CartItem, PaymentResult} from '@/types';
 import {paypal} from '@/lib/paypal';
-import { revalidatePath } from 'next/cache';
+import {revalidatePath} from 'next/cache';
 import {PAGE_SIZE} from '@/lib/constants';
+import {Prisma} from '@prisma/client';
 
 // Create order and the order items
 export async function createOrder() {
@@ -253,4 +254,52 @@ export async function getMyOrders({limit = PAGE_SIZE, page}: {limit?: number, pa
     data,
     totalPages: Math.ceil(dataCount / limit),
   };
+}
+
+type SalesDataType = {
+  month: string,
+  totalSales: number,
+}[];
+
+// Get  sales data and order summary
+export async function getOrderSummary() {
+  // Get counts for each sales
+  const ordersCount = await prisma.order.count();
+  const productsCount = await prisma.product.count();
+  const usersCount = await prisma.user.count();
+
+  // Calculate the total sales
+  const totalSales = await prisma.order.aggregate({
+    _sum: {totalPrice: true}
+  });
+
+  // Get monthly sales
+  const salesDataRaw = await prisma.$queryRaw<Array<{month: string, totalSales: Prisma.Decimal}>>`
+    SELECT to_char("createdAt", 'MM/YY') as "month", sum("totalPrice") as "totalSales"
+    FROM "Order"
+    GROUP BY to_char("createdAt", 'MM/YY')
+    `;
+
+  const salesData: SalesDataType = salesDataRaw.map((entry) => ({
+    month: entry.month,
+    totalSales: Number(entry.totalSales),
+  }));
+
+  // Get latest sales
+  const latestSales = await prisma.order.findMany({
+    orderBy: {createdAt: 'desc'},
+    include: {
+      user: {select: {name: true} },
+    },
+    take: 6,
+  });
+
+  return {
+    ordersCount,
+    productsCount,
+    usersCount,
+    totalSales,
+    latestSales,
+    salesData,
+  }
 }
