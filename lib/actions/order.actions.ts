3f7d6cc231a9939766d2d6190587caea72822b5b
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-04-14 17:27:16 +0300

    Admin orders page and action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -301,5 +301,22 @@ export async function getOrderSummary() {
     totalSales,
     latestSales,
     salesData,
-  }
+  };
+}
+
+// Get all orders for admin
+export async function getAllOrders({limit = PAGE_SIZE, page}: {limit?: number, page: number}) {
+  const data = await prisma.order.findMany({
+    orderBy: {createdAt: 'desc'},
+    take: limit,
+    skip: (page - 1) * limit,
+    include: {user: {select: {name: true}}},
+  });
+
+  const dataCount = await prisma.order.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
