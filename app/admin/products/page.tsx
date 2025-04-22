bf2a2350b5864b33be48982c9f304b226a7f6b5b
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-04-22 11:06:15 +0300

    Display products for admin

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,7 +2,9 @@ import {Metadata} from 'next';
 import Link from 'next/link';
 import {getAllProducts} from '@/lib/actions/product.actions';
 import {formatCurrency, formatId} from '@/lib/utils';
-
+import {Button} from '@/components/ui/button';
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from '@/components/ui/table';
+import Pagination from '@/components/shared/pagination';
 
 export const metadata:Metadata = {
     title: 'Admin Products',
@@ -20,17 +22,55 @@ const AdminProductsPage = async (props: { searchParams: Promise<{
     const category = searchParams.category || '';
 
     const products = await getAllProducts({
-        query: searchText,
-        page,
-        category,
+      query: searchText,
+      page,
+      category,
     });
-    console.log(products);
 
     return (
       <div className='space-y-2'>
         <div className='flex-between'>
           <h1 className='h2-bold'> Products</h1>
+          <Button asChild variant='default'>
+            <Link href='/admin/products/create'>Create Product</Link>
+          </Button>
         </div>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>NAME</TableHead>
+              <TableHead className='text-right'>PRICE</TableHead>
+              <TableHead>CATEGORY</TableHead>
+              <TableHead>STOCK</TableHead>
+              <TableHead>RATING</TableHead>
+              <TableHead className='w-[100px]'>ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {products.data.map(product => (
+              <TableRow key={product.id}>
+                <TableCell>{formatId(product.id)}</TableCell>
+                <TableCell>{product.name}</TableCell>
+                <TableCell className='text-right'>{formatCurrency(product.price)}</TableCell>
+                <TableCell>{product.category}</TableCell>
+                <TableCell>{product.stock}</TableCell>
+                <TableCell>{product.rating}</TableCell>
+                <TableCell className='flex gap-1'>
+                  <Button asChild variant='outline' size='sm'>
+                    <Link href={`/admin/products/${product.id}`}>Edit</Link>
+                  </Button>
+                  {/*DELETE*/}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+
+        {products.totalPages > 1 && (
+            <Pagination page={page} totalPages={products?.totalPages} />
+        )}
       </div>
     )
 };
