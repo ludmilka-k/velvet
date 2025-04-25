3e404c638d44278fd44f99841ece303137edbb53
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-04-25 15:26:21 +0300

    Orders Search

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -12,8 +12,10 @@ export const metadata:Metadata = {
     title: 'Admin Orders',
 }
 
-const AdminOrdersPage = async (props:{searchParams: Promise<{page: string}>}) => {
-    const {page = '1'} = await props.searchParams;
+const AdminOrdersPage = async (props:{
+    searchParams: Promise<{page: string, query: string}>
+}) => {
+    const {page = '1', query: searchText} = await props.searchParams;
 
     const session = await auth();
     if(session?.user?.role !== 'admin')
@@ -21,18 +23,29 @@ const AdminOrdersPage = async (props:{searchParams: Promise<{page: string}>}) =>
 
     const orders = await getAllOrders({
         page: Number(page),
-        // limit: 2,
+        query: searchText,
     });
 
     return (
       <div className='space-y-2'>
-        <h1>Orders</h1>
+          <div className='flex items-center gap-3'>
+              <h1 className='h2-bold'>Orders</h1>
+              {searchText && (
+                  <div>
+                      Filtered fy <i>&quot;{searchText}&quot;</i>  {' '}
+                      <Link href='/admin/orders'>
+                          <Button variant='outline' size='sm'>Remove Filter</Button>
+                      </Link>
+                  </div>
+              )}
+          </div>
         <div className='overflow-x-auto'>
           <Table>
             <TableHeader>
               <TableRow>
                 <TableHead>ID</TableHead>
                 <TableHead>DATE</TableHead>
+                <TableHead>BAYER</TableHead>
                 <TableHead>TOTAL</TableHead>
                 <TableHead>PAID</TableHead>
                 <TableHead>DELIVERED</TableHead>
@@ -44,6 +57,7 @@ const AdminOrdersPage = async (props:{searchParams: Promise<{page: string}>}) =>
                 <TableRow key={order.id}>
                   <TableCell>{formatId(order.id)}</TableCell>
                   <TableCell>{formatDateTime(order.createdAt).dateTime}</TableCell>
+                    <TableCell>{order.user.name}</TableCell>
                   <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
                   <TableCell>{order.isPaid && order.paidAt ? formatDateTime(order.paidAt).dateTime : 'Not Paid'}</TableCell>
                   <TableCell>{order.isDelivered && order.deliveredAt ? formatDateTime(order.deliveredAt).dateTime : 'Not Delivered'}</TableCell>
