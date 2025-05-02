e6333b6a5e559e41377856e0a0a9fc15952d843e
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2025-05-02 12:25:55 +0300

    Get reviews actions

--- lib/actions/review.actions.ts
+++ lib/actions/review.actions.ts
@@ -82,3 +82,30 @@ export async function createUpdateReview(data: z.infer<typeof insertReviewSchema
       return {success: false, message: formatError(error)};
     }
 }
+
+// Get all reviews for a product
+export async function getReviews({productId}: {productId: string}) {
+  const data = await prisma.review.findMany({
+    where: {productId: productId},
+    include: {
+      user: {select: {name: true}},
+    },
+    orderBy: {createdAt: 'desc'},
+  });
+
+  return {data};
+}
+
+// Get a review written by the current user
+export async function getReviewByProductId({productId}: {productId: string}) {
+  const session = await auth();
+
+  if (!session) throw new Error('User is not  authenticated');
+
+  return await prisma.review.findFirst({
+    where: {
+      productId,
+      userId: session?.user?.id,
+    },
+  });
+}
